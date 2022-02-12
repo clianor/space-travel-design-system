8012cb28626025c01127e63c2ec6dd7b5988634f
clianor <ejrgus94160@estsecurity.com>
2022-02-12 11:01:21 +0900

    @storybook/addon-postcss 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-postcss',
   ],
-  "framework": "@storybook/react",
+  framework: '@storybook/react',
   webpackFinal: async (config) => {
     Object.assign(config.resolve.alias, {
-      '@space-travel-design-system/ui': path.resolve(__dirname, '../packages/ui/src'),
+      '@space-travel-design-system/ui': path.resolve(
+        __dirname,
+        '../packages/ui/src',
+      ),
       '@space-travel-design-system/utils': path.resolve(
         __dirname,
         '../packages/utils/src',
-      )
+      ),
     });
 
     return config;
-  }
-}
+  },
+};
